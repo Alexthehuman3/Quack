17929a3d994c874ee393e7e87600e01fa0c6dc7a
Alex Wai Yiu Wong <aw987123@gmail.com>
2021-10-14 18:35:47 +0100

    Splitting Functions into Classes

---
+++ WindowConfig.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct WindowConfig
+{
+	const int Width = 800; //window width
+	const int Height = 600;//window height
+	/*
+	 * Note:
+	 * Background:
+	 * +1 = White
+	 * +2 = Grey
+	 * +3 = Black
+	 */
+};
